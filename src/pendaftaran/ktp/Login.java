761e0e584bdcbbd22020d7167a70ce6e71a2ce8a
Kukuh Alfian <hi@kalfian.com>
2020-07-20 21:14:18 +0700

    [REMOVE] Main Class on Login

--- src/pendaftaran/ktp/Login.java
+++ src/pendaftaran/ktp/Login.java
@@ -147,7 +147,7 @@ public class Login extends javax.swing.JFrame {
         String password = txtPassword.getText();
         Config db = new Config();
         try (Connection conn = db.getConnection()) {
-            PreparedStatement ps = conn.prepareStatement("SELECT * FROM user WHERE username=? AND password=?");
+            PreparedStatement ps = conn.prepareStatement("SELECT * FROM user WHERE username=? AND password=? LIMIT 1");
             ps.setString(1, username);
             ps.setString(2, password);
             ResultSet rs = ps.executeQuery();
@@ -156,47 +156,13 @@ public class Login extends javax.swing.JFrame {
                 dashboard.setVisible(true);
                 dispose();
             } else {
-                JOptionPane.showMessageDialog(this, "Kredensial salah !", "Error", JOptionPane.ERROR_MESSAGE);
+                JOptionPane.showMessageDialog(this, "Email / Password salah !", "Error", JOptionPane.ERROR_MESSAGE);
             }
         } catch (Exception e) {
             System.out.println("Error " + e.getMessage());
         }
     }//GEN-LAST:event_jButton1ActionPerformed
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new Login().setVisible(true);
-            }
-        });
-    }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
