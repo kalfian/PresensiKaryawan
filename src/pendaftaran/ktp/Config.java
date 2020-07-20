23fe2c4a23b5129b1310310e51f4ce2e99bd3b2c
Kukuh Alfian <kukuh@majoo.id>
2020-07-20 21:06:42 +0700

    [ADD] Fix Deprecated Config

--- src/pendaftaran/ktp/Config.java
+++ src/pendaftaran/ktp/Config.java
@@ -25,7 +25,7 @@ public class Config {
     
     public Connection getConnection (){
         try{
-            Class.forName("com.mysql.jdbc.Driver");
+            Class.forName("com.mysql.cj.jdbc.Driver");
         }
         catch(ClassNotFoundException e){
             System.err.println(e);  
--- src/pendaftaran/ktp/PendaftaranKTP.java
+++ src/pendaftaran/ktp/PendaftaranKTP.java
@@ -16,6 +16,8 @@ public class PendaftaranKTP {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Login login = new Login();
+        login.setVisible(true);
     }
     
 }
